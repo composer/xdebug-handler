9521986a0469ff7e616f3989bf4ffc0d4eb308b3
johnstevenson <john-stevenson@blueyonder.co.uk>
2017-11-22 21:58:35 +0000

    Minimal changes to decouple from composer

---
+++ tests/XdebugHandlerMock.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of composer/xdebug-handler.
+ *
+ * (c) Composer <https://github.com/composer>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Composer\XdebugHandler;
+
+class XdebugHandlerMock extends XdebugHandler
+{
+    public $restarted;
+    public $testVersion = '2.5.0';
+
+    public function __construct($loaded = null)
+    {
+        parent::__construct();
+
+        $loaded = null === $loaded ? true : $loaded;
+        $class = new \ReflectionClass(get_parent_class($this));
+
+        $prop = $class->getProperty('loaded');
+        $prop->setAccessible(true);
+        $prop->setValue($this, $loaded);
+
+        $prop = $class->getProperty('version');
+        $prop->setAccessible(true);
+        $version = $loaded ? $this->testVersion : '';
+        $prop->setValue($this, $version);
+
+        $this->restarted = false;
+    }
+
+    protected function restart($command)
+    {
+        $this->restarted = true;
+    }
+}
---
+++ tests/XdebugIniTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of composer/xdebug-handler.
+ *
+ * (c) Composer <https://github.com/composer>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Composer\XdebugHandler;
+
+use Composer\XdebugHandler\XdebugHandler;
+
+class IniHelperTest extends \PHPUnit\Framework\TestCase
+{
+    public static $envOriginal;
+
+    public function testWithNoIni()
+    {
+        $paths = array(
+            '',
+        );
+
+        $this->setEnv($paths);
+        $this->assertEquals($paths, XdebugHandler::getAllIniFiles());
+    }
+
+    public function testWithLoadedIniOnly()
+    {
+        $paths = array(
+            'loaded.ini',
+        );
+
+        $this->setEnv($paths);
+        $this->assertEquals($paths, XdebugHandler::getAllIniFiles());
+    }
+
+    public function testWithLoadedIniAndAdditional()
+    {
+        $paths = array(
+            'loaded.ini',
+            'one.ini',
+            'two.ini',
+        );
+
+        $this->setEnv($paths);
+        $this->assertEquals($paths, XdebugHandler::getAllIniFiles());
+    }
+
+    public function testWithoutLoadedIniAndAdditional()
+    {
+        $paths = array(
+            '',
+            'one.ini',
+            'two.ini',
+        );
+
+        $this->setEnv($paths);
+        $this->assertEquals($paths, XdebugHandler::getAllIniFiles());
+    }
+
+    public static function setUpBeforeClass()
+    {
+        // Save current state
+        self::$envOriginal = getenv(XdebugHandler::ENV_ORIGINAL);
+    }
+
+    public static function tearDownAfterClass()
+    {
+        // Restore original state
+        if (false !== self::$envOriginal) {
+            putenv(XdebugHandler::ENV_ORIGINAL.'='.self::$envOriginal);
+        } else {
+            putenv(XdebugHandler::ENV_ORIGINAL);
+        }
+    }
+
+    protected function setEnv(array $paths)
+    {
+        putenv(XdebugHandler::ENV_ORIGINAL.'='.implode(PATH_SEPARATOR, $paths));
+    }
+}
