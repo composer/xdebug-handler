19f7dd57149410df88788e10042a9fe365ac3f06
Alexey Kopytko <alexey@kopytko.com>
2019-05-27 02:57:01 +0900

    Update $_ENV if it was populated (#95)
    * Update environment changes in $_ENV if it is used
    
    $_SERVER is already updated when environment changes are made in a restarted process. This
    additionally updates $_ENV if it is being used.

--- src/Process.php
+++ src/Process.php
@@ -155,6 +155,15 @@ class Process
         } else {
             $_SERVER[$name] = $value;
         }
+        
+        if (false !== stripos((string) ini_get('variables_order'), 'E')) {
+            if ($unset) {
+                unset($_ENV[$name]);
+            } else {
+                $_ENV[$name] = $value;
+            }
+        }
+        
         return true;
     }
 }
